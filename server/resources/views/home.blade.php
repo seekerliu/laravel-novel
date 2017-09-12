7ea805e9eb8ffdbb2d873f0de7eb6e02305a5caa
seekerliu <seekerliu@vip.qq.com>
2017-09-13 00:08:59 +0800

    :lipstick: 添加全站CSS，完成首页样式

--- server/resources/views/home.blade.php
+++ server/resources/views/home.blade.php
@@ -3,18 +3,177 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="l-grid col-md-8">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">热门推荐</div>
 
                 <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
+                    <div class="element">
+                        <a href="http://shu000.com/books/10838" class="crop" title="圣墟"><img class="thumb" src="http://shu000.com/cover/10838_cover.jpg" border="0" alt="圣墟"></a>
+                        <div class="content">
+                            <a class="e-title" href="http://shu000.com/books/10838" title="圣墟">圣墟</a>
+                            <span class="e-view">热度: 14727</span>
                         </div>
-                    @endif
+                    </div>
+                    <div class="element">
+                        <a href="http://shu000.com/books/558" class="crop" title="大主宰"><img class="thumb" src="http://shu000.com/cover/558_cover.jpg" border="0" alt="大主宰"></a>
+                        <div class="content">
+                            <a class="e-title" href="http://shu000.com/books/558" title="大主宰">大主宰</a>
+                            <span class="e-view">热度: 46147</span>
+                        </div>
+                    </div>
+                    <div class="element">
+                        <a href="http://shu000.com/books/294" class="crop" title="完美世界"><img class="thumb" src="http://shu000.com/cover/294_cover.jpg" border="0" alt="完美世界"></a>
+                        <div class="content">
+                            <a class="e-title" href="http://shu000.com/books/294" title="完美世界">完美世界</a>
+                            <span class="e-view">热度: 67787</span>
+                        </div>
+                    </div>
+                    <div class="element">
+                        <a href="http://shu000.com/books/368" class="crop" title="雪鹰领主"><img class="thumb" src="http://shu000.com/cover/368_cover.jpg" border="0" alt="雪鹰领主"></a>
+                        <div class="content">
+                            <a class="e-title" href="http://shu000.com/books/368" title="雪鹰领主">雪鹰领主</a>
+                            <span class="e-view">热度: 27408</span>
+                        </div>
+                    </div>
+                    <div class="element">
+                        <a href="http://shu000.com/books/10796" class="crop" title="玄界之门"><img class="thumb" src="http://shu000.com/cover/10796_cover.jpg" border="0" alt="玄界之门"></a>
+                        <div class="content">
+                            <a class="e-title" href="http://shu000.com/books/10796" title="玄界之门">玄界之门</a>
+                            <span class="e-view">热度: 32176</span>
+                        </div>
+                    </div>
+                    <div class="element">
+                        <a href="http://shu000.com/books/10792" class="crop" title="一念永恒"><img class="thumb" src="http://shu000.com/cover/10792_cover.jpg" border="0" alt="一念永恒"></a>
+                        <div class="content">
+                            <a class="e-title" href="http://shu000.com/books/10792" title="一念永恒">一念永恒</a>
+                            <span class="e-view">热度: 28619</span>
+                        </div>
+                    </div>
+                    <div class="element">
+                        <a href="http://shu000.com/books/10802" class="crop" title="天影"><img class="thumb" src="http://shu000.com/cover/10802_cover.jpg" border="0" alt="天影"></a>
+                        <div class="content">
+                            <a class="e-title" href="http://shu000.com/books/10802" title="天影">天影</a>
+                            <span class="e-view">热度: 14850</span>
+                        </div>
+                    </div>
+                    <div class="element">
+                        <a href="http://shu000.com/books/11698" class="crop" title="龙王传说"><img class="thumb" src="http://shu000.com/cover/11698_cover.jpg" border="0" alt="龙王传说"></a>
+                        <div class="content">
+                            <a class="e-title" href="http://shu000.com/books/11698" title="龙王传说">龙王传说</a>
+                            <span class="e-view">热度: 16326</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-                    You are logged in!
+        <div class="l-right box col-md-4">
+            <div class="panel panel-default">
+                <div class="panel-heading">排行热榜</div>
+
+                <div class="panel-body">
+                    <ul class="content">
+                        <li>
+                            <div class="fll">
+                                <a href="http://shu000.com/books/3894" class="crop" title="仙逆">
+                                    <img class="thumb-s" title="仙逆" alt="仙逆" src="http://shu000.com/cover/3894_cover.jpg">
+                                </a>
+                            </div>
+                            <div class="l-right-info">
+                                <a class="r-title" href="http://shu000.com/books/3894" title="仙逆">仙逆</a>
+                                <a href="http://shu000.com/authors/3347" title="耳根" class="e-user">耳根</a>
+                                <span class="e-view">热度: 5684156</span>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="fll">
+                                <a href="http://shu000.com/books/7799" class="crop" title="开艘航母去抗日">
+                                    <img class="thumb-s" title="开艘航母去抗日" alt="开艘航母去抗日" src="http://shu000.com/cover/7799_cover.jpg">
+                                </a>
+                            </div>
+                            <div class="l-right-info">
+                                <a class="r-title" href="http://shu000.com/books/7799" title="开艘航母去抗日">开艘航母去抗日</a>
+                                <a href="http://shu000.com/authors/6718" title="且听沧海" class="e-user">且听沧海</a>
+                                <span class="e-view">热度: 112145</span>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="fll">
+                                <a href="http://shu000.com/books/7815" class="crop" title="花间高手">
+                                    <img class="thumb-s" title="花间高手" alt="花间高手" src="http://shu000.com/cover/7815_cover.jpg">
+                                </a>
+                            </div>
+                            <div class="l-right-info">
+                                <a class="r-title" href="http://shu000.com/books/7815" title="花间高手">花间高手</a>
+                                <a href="http://shu000.com/authors/6730" title="冷云邪神" class="e-user">冷云邪神</a>
+                                <span class="e-view">热度: 89743</span>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="fll">
+                                <a href="http://shu000.com/books/3851" class="crop" title="凡人修仙传">
+                                    <img class="thumb-s" title="凡人修仙传" alt="凡人修仙传" src="http://shu000.com/cover/3851_cover.jpg">
+                                </a>
+                            </div>
+                            <div class="l-right-info">
+                                <a class="r-title" href="http://shu000.com/books/3851" title="凡人修仙传">凡人修仙传</a>
+                                <a href="http://shu000.com/authors/3283" title="忘语" class="e-user">忘语</a>
+                                <span class="e-view">热度: 86081</span>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="fll">
+                                <a href="http://shu000.com/books/6519" class="crop" title="官仙">
+                                    <img class="thumb-s" title="官仙" alt="官仙" src="http://shu000.com/cover/6519_cover.jpg">
+                                </a>
+                            </div>
+                            <div class="l-right-info">
+                                <a class="r-title" href="http://shu000.com/books/6519" title="官仙">官仙</a>
+                                <a href="http://shu000.com/authors/3312" title="陈风笑" class="e-user">陈风笑</a>
+                                <span class="e-view">热度: 81374</span>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="fll">
+                                <a href="http://shu000.com/books/5378" class="crop" title="官道无疆">
+                                    <img class="thumb-s" title="官道无疆" alt="官道无疆" src="http://shu000.com/cover/5378_cover.jpg">
+                                </a>
+                            </div>
+                            <div class="l-right-info">
+                                <a class="r-title" href="http://shu000.com/books/5378" title="官道无疆">官道无疆</a>
+                                <a href="http://shu000.com/authors/3095" title="瑞根" class="e-user">瑞根</a>
+                                <span class="e-view">热度: 81244</span>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="fll">
+                                <a href="http://shu000.com/books/7774" class="crop" title="医道官途">
+                                    <img class="thumb-s" title="医道官途" alt="医道官途" src="http://shu000.com/cover/7774_cover.jpg">
+                                </a>
+                            </div>
+                            <div class="l-right-info">
+                                <a class="r-title" href="http://shu000.com/books/7774" title="医道官途">医道官途</a>
+                                <a href="http://shu000.com/authors/6428" title="石章鱼" class="e-user">石章鱼</a>
+                                <span class="e-view">热度: 73640</span>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="fll">
+                                <a href="http://shu000.com/books/294" class="crop" title="完美世界">
+                                    <img class="thumb-s" title="完美世界" alt="完美世界" src="http://shu000.com/cover/294_cover.jpg">
+                                </a>
+                            </div>
+                            <div class="l-right-info">
+                                <a class="r-title" href="http://shu000.com/books/294" title="完美世界">完美世界</a>
+                                <a href="http://shu000.com/authors/291" title="辰东" class="e-user">辰东</a>
+                                <span class="e-view">热度: 67789</span>
+                            </div>
+                        </li>
+                        <li class="l-right-end">
+                            <a rel="nofollow" href="#" class="box-more">查看更多</a>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
